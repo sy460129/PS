11f78f4af6ff5a5dc22002af1cb185bbefd0bd00
sy46 <tmddus0129@naver.com>
2022-09-17 15:33:58 +0900

    1012.cpp

--- 1011.cpp
+++ 1011.cpp
@@ -1,24 +1,47 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
 using namespace std;
-int main(){
-	int n,x,y;
-	cin >> n;
-	while(n--){
-		cin >> x >> y;
-		y-=x;
-		x=sqrt(y);
-		if(x*x==y){
-			cout << x*2-1;
+int arr[51][51];
+bool check[51][51];
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int n, m, k;
+void dfs(int x, int y) {
+	check[x][y] = true;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (nx<0 || ny<0 || nx>m-1 || ny>n-1) {
+			continue;
 		}
-		else{
-			if(x*x+x>=y){
-				cout << x*2;
-			}
-			else{
-				cout << x*2+1;
+		if (arr[nx][ny] == 1 && !check[nx][ny]) {
+			dfs(nx, ny);
+		}
+	}
+}
+int main() {
+	int a, b, c;
+	cin >> c;
+	while (c--) {
+		int bug = 0;
+		cin >> m >> n >> k;
+		for (int i = 0; i < k; i++) {
+			cin >> a >> b;
+			arr[a][b] = 1;
+		}
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 1 && !check[i][j]) {
+					dfs(i, j);
+					bug++;
+				}
 			}
 		}
-		cout << "\n";
+		cout << bug << "\n";
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = 0;
+				check[i][j] = false;
+			}
+		}
 	}
 }
