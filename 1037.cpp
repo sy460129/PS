b16c419efaabc8652595da8697361653fadbbae7
sy46 <tmddus0129@naver.com>
2022-09-17 15:53:56 +0900

    1037.cpp

---
+++ 1037.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	vector<int>v;
+	int n, x;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+	cout << v.front() * v.back();
+}
