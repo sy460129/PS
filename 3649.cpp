9044c0e5c62804c912a84d1c0241e1a9d1fd8182
sy46 <tmddus0129@naver.com>
2022-09-21 10:02:01 +0900

    3649.cpp

--- 3649.cpp
+++ 3649.cpp
@@ -8,37 +8,37 @@ int main() {
 	while (cin >> x) {
 		cin >> n;
 		bool check = false;
-		x *= 10000000;
-		int* arr = new int[n];
-		for (int i = 0; i < n; i++) {
+		x*=10000000;
+		int* arr=new int[n];
+		for(int i=0; i<n; i++) {
 			cin >> arr[i];
 		}
-		sort(arr, arr + n);
-		int f = 0, e = n - 1, c = 0;
+		sort(arr, arr+n);
+		int f=0, e=n-1, c=0;
 		while (1) {
-			int mid = (f + e) / 2;
-			if (arr[c] + arr[mid] == x) {
-				if (c != mid) {
+			int mid=(f+e)/2;
+			if(arr[c]+arr[mid]==x) {
+				if(c!=mid) {
 					cout << "yes " << arr[c] << " " << arr[mid] << "\n";
-					check = true;
-					c = n;
+					check=true;
+					c=n;
 				}
 				else {
 					f++;
 				}
 			}
-			else if (arr[c] + arr[mid] > x) {
-				e = mid - 1;
+			else if(arr[c]+arr[mid]>x) {
+				e=mid-1;
 			}
-			else if (arr[c] + arr[mid] < x) {
-				f = mid + 1;
+			else if(arr[c]+arr[mid]<x) {
+				f=mid+1;
 			}
-			if (f > e) {
+			if(f>e) {
 				c++;
-				f = 0, e = n - 1;
+				f=0, e=n-1;
 			}
-			if (c >= n) {
-				if (!check) {
+			if(c>=n) {
+				if(!check) {
 					cout << "danger\n";
 				}
 				break;
