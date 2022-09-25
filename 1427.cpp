30f281f7ab7b333a2060412077ce03b0b61bd0d2
sy46 <tmddus0129@naver.com>
2022-09-25 11:03:31 +0900

    1427.cpp

--- 1427.cpp
+++ 1427.cpp
@@ -2,16 +2,16 @@
 #include<vector>
 #include<algorithm>
 int compare(int a, int b) {
-	return a > b;
+	return a>b;
 }
 using namespace std;
 int main() {
 	int n,x;
 	vector<int>v;
 	cin >> n;
-	while (n != 0) {
-		x = n % 10;
-		n /= 10;
+	while (n!=0) {
+		x=n%10;
+		n/=10;
 		v.push_back(x);
 	}
 	sort(v.begin(), v.end(), compare);
