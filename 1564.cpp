0cdcf836815311da7eca00a0bc509b3bc74e2afd
sy46 <tmddus0129@naver.com>
2022-09-25 11:01:24 +0900

    1564.cpp

--- 1564.cpp
+++ 1564.cpp
@@ -8,16 +8,14 @@ int main() {
 	int n;
 	string s;
 	cin >> n;
-	for (int i = 2; i <= n; i++) {
-		dp[i] = dp[i - 1] * i;
-		while (dp[i] % 10 == 0) {
-			dp[i] /= 10;
-		}
-		dp[i] %= 1000000000000;
+	for (int i=2; i<=n; i++) {
+		dp[i]=dp[i-1]*i;
+		while (dp[i]%10==0) dp[i]/=10;
+		dp[i]%=1000000000000;
 	}
-	for (int i = 0; i < 5; i++) {
-		s += to_string(dp[n] % 10);
-		dp[n] /= 10;
+	for (int i=0; i<5; i++) {
+		s+=to_string(dp[n] % 10);
+		dp[n]/=10;
 	}
 	reverse(s.begin(), s.end());
 	cout << s;
