f7d1df268affee878d7eb9f42bdd48121d4d3cd1
sy46 <tmddus0129@naver.com>
2022-10-02 12:26:09 +0900

    14501.cpp

--- ps.cpp
+++ ps.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
-#include <tuple>
-#include <queue>
 using namespace std;
-using ll=long long;
-using t=tuple<ll, ll, ll>;
-priority_queue<t, vector<t>, greater<t>>q;
-int arr[10001];
-int find(int x){
-    if(x==arr[x]){
-        return x;
-    }
-    else{
-        return x=find(arr[x]);
-    }
+char arr[201];
+void preOrder(int i){
+    cout << arr[i];
+    preOrder(arr[i*2]);
+    preOrder(arr[i*2+1]);
 }
-bool merge(int a, int b){
-    a=find(a);
-    b=find(b);
-    if(a==b) return true;
-    a>b ? arr[a]=b : arr[b]=a;
-    return false;
+void prOrder(int i){
+    preOrder(arr[i*2]);
+    cout << arr[i];
+    preOrder(arr[i*2+1]);
+}
+void pOrder(int i){
+    preOrder(arr[i*2]);
+    preOrder(arr[i*2+1]);
+    cout << arr[i];
 }
 int main(){
-    ll n, m, a, b, c, cnt=0; cin >> n >> m;
-    while(m--){
-        cin >> a >> b >> c;
-        q.push({c,a,b});
-    }
+    int n;
+    char x, y, z;
+    cin >> n;
     for(int i=1; i<=n; i++){
-        arr[i]=i;
+        cin >> x >> y >> z;
+        arr[i]=x-'A';
+        if(y!='.') arr[i*2]=y-'A';
+        if(z!='.') arr[i*2+1]=z-'A';
     }
-    while(!q.empty()){
-        auto cur=q.top(); q.pop();
-        ll x=get<0>(cur);
-        ll y=get<1>(cur);
-        ll z=get<2>(cur);
-        if(!merge(y, z)) cnt+=x;
-    }
-    cout << cnt;
+    preOrder(arr[1]);
+    prOrder(arr[1]);
+    pOrder(arr[1]);
 }
