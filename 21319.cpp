831c1c2c8e14f306ce27f51ae1460b143df566c5
sy46 <tmddus0129@naver.com>
2022-10-06 10:28:51 +0900

    21319.cpp

--- 21319.cpp
+++ 21319.cpp
@@ -5,6 +5,8 @@ int dp[200001];
 int arr[200001];
 stack<int>st;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     int n; cin >> n;
     for(int i=1; i<=n; i++){
         cin >> dp[i];
