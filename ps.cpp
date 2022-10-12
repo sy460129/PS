07773ed7542266187085b326c3faf64b541b2399
sy46 <tmddus0129@naver.com>
2022-10-12 14:35:16 +0900

    최대공약수

--- ps.cpp
+++ ps.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int dp[1002];
-int card[1002];
+using ull=unsigned long long;
+ull arr[200001];
+ull gcd(ull a, ull b){
+	return b ? gcd(b, a%b) : a;
+}
 int main(){
-	int n; cin >> n;
-	for(int i=1; i<=n; i++){
-		cin >> card[i];
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	ull n, m, x, g=0;
+	cin >> n >> m;
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
 	}
-	for(int i=1; i<=n; i++){
-		dp[i]=card[i];
-		for(int j=1; j<i; j++){
-			dp[i]=max(dp[i], dp[j]+card[i-j]);
-		}
+	sort(arr, arr+n);
+	for(int i=1; i<n; i++){
+		g=gcd(arr[i]-arr[0], g);
 	}
-	cout << dp[n];
+	for(int i=0; i<m; i++){
+		cin >> x;
+		cout << gcd(arr[0]+x, g) << " ";
+	}
 }
