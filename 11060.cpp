0475246d31ad9157bffd88b30ebc20131632648c
sy46 <tmddus0129@naver.com>
2022-10-18 22:00:09 +0900

    점프 점프

--- 11060.cpp
+++ 11060.cpp
@@ -7,15 +7,11 @@ int cnt=2147483647;
 int main(){
     int n; cin >> n;
     rep(i, 1, n) cin >> arr[i];
-    if(n==1 && arr[1]==0){
-        cout << 0;
-        return 0;
-    }
     dp[1]=0;
     rep(i, 1, n){
         if(i!=1 && dp[i]==0) continue;
-        rep(j, 1, arr[i]){
-            if(i+j>=n) cnt=min(cnt, dp[i]+1);
+        rep(j, 0, arr[i]){
+            if(i+j>=n) cnt=min(cnt, dp[i]);
             else{
                 if(dp[i+j]==0) dp[i+j]=dp[i]+1;
             }
