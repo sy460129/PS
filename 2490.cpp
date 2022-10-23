996b0ebb868594b08bf846df96762eafa6e6e946
sy46 <tmddus0129@naver.com>
2022-10-23 11:09:49 +0900

    윷놀이

--- 2490.cpp
+++ 2490.cpp
@@ -13,3 +13,37 @@ int main(){
         r=0;
     }
 }
+
+/*
+#include <iostream>
+#define rep(i, a, b) for(int i=a; i<b; i++)
+using namespace std;
+int r, a;
+int main(){
+    rep(i, 0, 3){
+        rep(j, 0, 4){
+            cin >> a;
+            r+=a;
+        }
+        switch(r){
+			case 3:
+				cout << "A";
+				break;
+			case 2:
+				cout << "B";
+				break;
+			case 1:
+				cout << "C";
+				break;
+			case 0:
+				cout << "D";
+				break;
+			default:
+				cout << "E";
+				break;
+		}
+		cout << "\n";
+		r=0;
+	}
+}
+*/
--- ps.cpp
+++ ps.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+#define rep(i, a, b) for(int i=a; i<b; i++)
 using namespace std;
-int arr[100001];
-int ask(int a){
-	int x;
-	cout << "? " << a << "\n";
-	cin >> x;
-	arr[a]=x;
-	return x;
-}
+int r, a;
 int main(){
-	int n; cin >> n;
-	int f=1, e=n;
-	arr[0]=n+1, arr[n+1]=n+1;
-	while(f<e){
-		int m=(f+e)/2;
-		ask(m);
-		ask(m+1);
-		if(arr[m]<arr[m+1]){
-			e=m;
+    rep(i, 0, 3){
+        rep(j, 0, 4){
+            cin >> a;
+            r+=a;
+        }
+        switch(r){
+			case 3:
+				cout << "A";
+				break;
+			case 2:
+				cout << "B";
+				break;
+			case 1:
+				cout << "C";
+				break;
+			case 4:
+				cout << "D";
+				break;
+			case 0:
+				cout << "E";
+				break;
 		}
-		else f=m+1;
+		cout << "\n";
+		r=0;
 	}
-	cout << "! " << e;
-	cout.flush();
 }
