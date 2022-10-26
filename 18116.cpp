312d43fd825e6a562566d1d3654a44cadbe165d1
sy46 <tmddus0129@naver.com>
2022-10-26 18:38:30 +0900

    로봇 조립

---
+++ 18116.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int p[1000001];
+int dp[1000001];
+int find(int x){
+    if(x==p[x]) return x;
+    else return x=find(p[x]);
+}
+void merge(int x, int y){
+    x=find(p[x]);
+    y=find(p[y]);
+    if(x!=y){
+        x>y ? p[x]=y, dp[y]+=dp[x] : p[y]=x, dp[x]+=dp[y];
+    }
+}
+int f(int x){
+    x=find(x);
+    return dp[x];
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    char c;
+    int n, x, y;
+    cin >> n;
+    for(int i=1; i<=1000001; i++){
+        p[i]=i, dp[i]=1;
+    }
+    while(n--){
+        cin >> c;
+        if(c=='I'){
+            cin >> x >> y;
+            merge(x, y);
+        }
+        else {
+            cin >> x;
+            cout << f(x) << "\n";
+        }
+    }
+}
--- ps.cpp
+++ ps.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    vector<int>v;
+    set<int>s;
     int m, n, l, x, y, cnt=0;
     cin >> m >> n >> l;
     while(m--){
         cin >> x;
-        v.push_back(x);
+        s.insert(x);
     }
-    sort(v.begin(), v.end());
     while(n--){
         cin >> x >> y;
         if(y<=l) {
-            auto it=lower_bound(v.begin(), v.end(), x)-v.begin();
-            if(it==v.size()){
-                if(x-v[it-1]+y<=l) cnt++;
-            }
-            if(abs(x-v[it])+y<=l) cnt++;
-            else if(it!=0 && x-v[it-1]+y<=l) cnt++;
+            auto it=s.lower_bound(x);
+            if(abs((*it)-x)+y<=l || (it!=s.begin() && abs(x-(*(--it)))+y<=l)) cnt++;
         }
     }
     cout << cnt;
