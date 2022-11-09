4bba64e5f43848a38803060a5cd64952e4b0114b
sy46 <tmddus0129@naver.com>
2022-11-09 10:31:05 +0900

    리모컨

--- ps.cpp
+++ ps.cpp
@@ -1,36 +1,62 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <cmath>
-#define MAX 9876543211
 using namespace std;
-using ll = long long;
-vector<ll>v;
-bool c;
-int main() {
-    int n; cin >> n;
-    if(n<=10){
-        cout << n;
+bool visit[1000000];
+bool check[10];
+bool ch, cn, cx;
+int n, x, t, c, s, p, cnt;
+int main(){
+    cin >> c >> n;
+    while(n--){
+        cin >> x;
+        check[x]=true;
+    }
+    visit[100]=true;
+    if(!check[0]) visit[0]=true;
+    if(visit[0] && c==0){
+        cout << 1;
         return 0;
     }
-    n-=10;
-    ll t = 20;
-    while(n!=1){
-        string s=to_string(t);
-        c=true;
-        for(int i=1; i<s.length(); i++) {
-            if(s[i-1]-'0'<=s[i]-'0') {
-                t+=pow(10, s.length()-i);
-                t-=((s[i]-'0')*pow(10, s.length()-i-1));
-                c=false;
+    for(int i=1; i<=999999; i++){
+        x=i;
+        ch=true;
+        while(x!=0){
+            t=x%10;
+            x/=10;
+            if(check[t]){
+                ch=false;
                 break;
             }
         }
-        if(c) n--, t++;
-        if(t>MAX){
-            cout << -1;
-            return 0;
-        }
+        if(ch) visit[i]=true;
     }
-    cout << t;
+    t=0;
+    n=c, x=c;
+    while(1){
+        if(n<=999999 && visit[n]){
+            s=n;
+            while(s!=0){
+                s/=10;
+                t++;
+            }
+            cn=true;
+        }
+        if(x>=0 && visit[x]){
+            s=x;
+            while(s!=0){
+                s/=10;
+                p++;
+            }
+            p=max(1,p);
+            cx=true;
+        }
+        if(cn || cx) break;
+        n++, x--;
+    }
+    if(cn && cx){
+        cout << min(n-c+t, min(c-x+p, abs(100-c)));
+    }
+    else{
+        if(cn) cout << min(n-c+t, abs(100-c));
+        else cout <<  min(c-x+p, abs(100-c));
+    }
 }
