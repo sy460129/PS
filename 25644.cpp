143672eddbded2583e728b27d33c6a38ab16e437
sy46 <tmddus0129@naver.com>
2022-12-04 18:26:05 +0900

    최대 상승

--- 25644.cpp
+++ 25644.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main(){
 	int n, x, mi=2147483647, ma=0, ans=0; cin >> n;
-	vector<int>v;
 	for(int i=0; i<n; i++){
 		cin >> x;
 		ma=max(ma, x);
