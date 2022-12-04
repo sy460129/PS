ecbcbf017f9f1ce2d17ca15b6eb6efe821a38a0e
sy46 <tmddus0129@naver.com>
2022-12-04 15:13:59 +0900

    민트 초코

--- 20302.cpp
+++ 20302.cpp
@@ -2,56 +2,56 @@
 #include <cmath>
 using namespace std;
 int check[100001];
-int main(){
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 	char c;
 	int n, s; cin >> n;
-	for(int i=0; i<n; i++){
-        int j=2;
-		if(i==0){
+	for (int i = 0; i < n; i++) {
+		int j = 2;
+		if (i == 0) {
 			cin >> s;
-			if(s<0) s*=-1;
-            while(j<=sqrt(s)){
-                if(s%j==0){
+			if (s < 0) s *= -1;
+			while (j <= sqrt(s)) {
+				if (s % j == 0) {
 					check[j]++;
-                    s/=j;
+					s /= j;
 				}
-                else j++;
-            }
-            check[s]++;
+				else j++;
+			}
+			check[s]++;
 		}
-		else{
+		else {
 			cin >> c >> s;
-			if(s<0) s*=-1;
-			if(c=='*'){
-                while(j<=sqrt(s)){
-                    if(s%j==0){
-				    	check[j]++;
-                        s/=j;
-				    }
-                    else j++;
-                }
-                check[s]++;
+			if (s < 0) s *= -1;
+			if (c == '*') {
+				while (j <= sqrt(s)) {
+					if (s % j == 0) {
+						check[j]++;
+						s /= j;
+					}
+					else j++;
+				}
+				check[s]++;
 			}
-			else{
-				while(j<=sqrt(s)){
-                    if(s%j==0){
-				    	check[j]--;
-                        s/=j;
-				    }
-                    else j++;
-                }
-                check[s]--;
+			else {
+				while (j <= sqrt(s)) {
+					if (s % j == 0) {
+						check[j]--;
+						s /= j;
+					}
+					else j++;
+				}
+				check[s]--;
 			}
 		}
-        if(s==0){
-            cout << "mint chocolate";
-            return 0;
-        }
+		if (s == 0) {
+			cout << "mint chocolate";
+			return 0;
+		}
 	}
-	for(int i=2; i<=100000; i++){
-		if(check[i]<0){
+	for (int i = 2; i <= 100000; i++) {
+		if (check[i] < 0) {
 			cout << "toothpaste";
 			return 0;
 		}
