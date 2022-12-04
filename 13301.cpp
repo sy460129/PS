d5ff36036426df010701bad9a5ae0f5948e906c0
sy46 <tmddus0129@naver.com>
2022-12-04 11:43:46 +0900

    타일 장식물

--- 13301.cpp
+++ 13301.cpp
@@ -4,8 +4,7 @@ using ll=long long;
 ll dp[81];
 int main(){
 	int n; cin >> n;
-    dp[1]=1;
-    dp[2]=1;
+    dp[1]=1, dp[2]=1;
     for(int i=3; i<=n; i++) dp[i]=dp[i-1]+dp[i-2];
     cout << dp[n]*4+dp[n-1]*2;
 }
