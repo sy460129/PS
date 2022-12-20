25f96766cf1a7ffdef78f4e1a882ccaf699db8fb
sy46 <tmddus0129@naver.com>
2022-12-20 15:43:53 +0900

    동전 1

--- 2293.cpp
+++ 2293.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int arr[101];
 int dp[10001];
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     int n, k; cin >> n >> k;
     for(int i=0; i<n; i++) cin >> arr[i];
     sort(arr, arr+n);
@@ -13,5 +15,5 @@ int main(){
             dp[j]+=dp[j-arr[i]];
         }
     }
-    cout << dp[i] << " ";
+    cout << dp[k] << " ";
 }
