bcbd142779d31ea1845486bc7973e8c8dbfeb090
sy46 <tmddus0129@naver.com>
2023-01-03 10:19:54 +0900

    연속인가? ?

---
+++ 26517.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+int main() {
+	long long x, a, b, c, d; cin >> x >> a >> b >> c >> d;
+	if(a*x+b==c*x+d) cout << "Yes " << a*x+b;
+	else cout << "No";
+}
--- ps.cpp
+++ ps.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 int main() {
-	int x, a, b, c, d; cin >> x >> a >> b >> c >> d;
+	long long x, a, b, c, d; cin >> x >> a >> b >> c >> d;
 	if(a*x+b==c*x+d) cout << "Yes " << a*x+b;
 	else cout << "No";
 }
