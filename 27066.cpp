41853d057abe49d162ae78ba34dee04bb39b012e
sy46 <tmddus0129@naver.com>
2023-01-03 12:52:27 +0900

    나무 블럭 게임

--- 27066.cpp
+++ 27066.cpp
@@ -4,19 +4,17 @@
 #include <iomanip>
 using namespace std;
 int main(){
-	int n,  x, y; cin >> n;
+	int n; cin >> n;
 	double t=2, res=0, ans;
 	vector<double>v(n);
 	for(int i=0; i<n; i++) cin >> v[i];
 	sort(v.rbegin(), v.rend());
 	ans=v[v.size()/2];
-	x=v.back();
+	res=v.back();
 	v.pop_back();
-	res=x;
 	while(!v.empty()){
-		y=v.back();
+		res+=v.back();
 		v.pop_back();
-		res+=y;
 		v.push_back(res/t);
 		ans=max(ans, v[v.size()/2]);
 		v.pop_back();
