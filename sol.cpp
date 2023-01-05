e704f7ef6f657a87d9f6402baf661431692dead3
sy46 <tmddus0129@naver.com>
2023-01-05 22:24:59 +0900

    아기 상어

--- sol.cpp
+++ sol.cpp
@@ -12,31 +12,31 @@ bool visit[21][21];
 int dx[]={-1,0,1,0};
 int dy[]={0,-1,0,1};
 vector<pair<int,int>>v;
-queue<pair<pair<int,int>,pair<int,int>>>q;
+queue<tuple<int,int,int>>q;
 void bfs(){
 	while(!q.empty()){
-		int x=q.front().first.first; // x좌표
-		int y=q.front().first.second; // y좌표
-		int w=q.front().second.first; // 상어 크기
-		int z=q.front().second.second+1; // 이동 횟수
+        auto cur=q.front(); q.pop();
+		int x=get<0>(cur);
+        int y=get<1>(cur);
+        int z=get<2>(cur);
 		while(num[p]==0){
 			p++;
-			if(p>=w) c=true;
+			if(p>=z) c=true;
 		}
 		if(c) break;
 		q.pop();
 		for(int i=0; i<4; i++){
 			int nx=x+dx[i];
 			int ny=y+dy[i];
-			if(nx<0 || ny<0 || nx>n-1 || ny>n-1 || visit[nx][ny] || arr[nx][ny]>w || (now!=0 && now!=z)) continue;
-			if(arr[nx][ny]<w && arr[nx][ny]!=0 && !visit[nx][ny]){
+			if(nx<0 || ny<0 || nx>n-1 || ny>n-1 || visit[nx][ny] || arr[nx][ny]>z) continue;
+			if(arr[nx][ny]<z && arr[nx][ny]!=0 && !visit[nx][ny]){
 				v.push_back({nx,ny});
 				now=z;
 				visit[nx][ny]=true;
 			}
 			else{
 				if(v.empty()){
-					q.push({{nx,ny},{w,z}});
+					q.push({nx,ny,z});
 					visit[nx][ny]=true;
 				}
 			}
@@ -45,11 +45,12 @@ void bfs(){
 			sort(v.begin(), v.end());
 			int a=v[0].first, b=v[0].second;
 			ans=now, t++;
+			cout << a << " " << b << " " << ans << "\n";
 			while(!q.empty()) q.pop();
 			num[arr[a][b]]--;
 			arr[a][b]=0;
-			if(t==w) w++, t=0;
-			q.push({{a, b},{w,now}});
+			if(t==z) z++, t=0;
+			q.push({a,b,z});
 			fill(visit[0], visit[n], false);
 			v.clear();
 			now=0;
@@ -62,7 +63,7 @@ int main(){
 		for(int j=0; j<n; j++){
 			cin >> arr[i][j];
 			if(arr[i][j]==9) {
-				q.push({{i,j},{2,0}});
+				q.push({i,j,2});
 				arr[i][j]=0;
 				visit[i][j]=true;
 			}
