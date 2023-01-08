a628ac9bfa69d6322f59234fbd1654c6af18edc3
sy46 <tmddus0129@naver.com>
2023-01-08 14:37:30 +0900

    미세먼지 안녕!

--- 17144.cpp
+++ 17144.cpp
@@ -91,10 +91,8 @@ int main(){
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            cout << arr[i][j] << " ";
             ans+=arr[i][j];
         }
-        cout << "\n";
     }
     cout << ans+2;
 }
