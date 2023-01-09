3f7ca903332e35a4ccb2bc0972087ef8adebbf0e
sy46 <tmddus0129@naver.com>
2023-01-09 12:06:39 +0900

    특식 배부

--- ps.cpp
+++ ps.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+#include <cmath>
 using namespace std;
-string p(string s, string c){
-    string ans;
-    int y=0;
-    for(int i=0; i<s.length(); i++){
-        ans+=to_string((((s[i]-'0')+(c[i]-'0'))+y)%10);
-        y=(((s[i]-'0')+(c[i]-'0'))+y)/10;
-    }
-    if(y==1) ans+="1";
-    reverse(ans.begin(), ans.end());
-    return ans;
+int cnt=0, h, r, c;
+void dv(int x, int y, int n) {
+	/*if(x==r && y==c){
+		cout << cnt;
+		exit(0);
+	}*/
+	if(n<=2){
+		for (int i=x; i<n+x; i++){
+			for (int j=y; j<n+y; j++){
+				if(i==r && j==c){
+					cout << cnt;
+					exit(0);
+				}
+				else{
+					cnt++;
+				}
+			}
+		}
+	}
+	else {
+		if(r<x+n/2 && c<y+n/2){
+			dv(x, y, n/2);
+		}
+		cnt+=n*(n/4);
+		if(r<x+n/2 && c>=y+n/2){
+			dv(x, n/2+y, n/2);
+		}
+		cnt+=n*(n/4);
+		if(r>=x+n/2 && c<y+n/2){
+			dv(n/2+x, y, n/2);
+		}
+		cnt+=n*(n/4);
+		if(r>=x+n/2 && c>=y+n/2){
+			dv(n/2+x, n/2+y, n/2);
+		
+		}
+	}
 }
-int main(){
-    string s, c, a; cin >> s >> c;
-    reverse(s.begin(), s.end());
-    reverse(c.begin(), c.end());
-    if(s.length()>c.length()){
-        while(s.length()!=c.length()) c+="0";
-    }
-    else{
-        while(s.length()!=c.length()) s+="0";
-    }
-    cout << p(s, c);
+int main() {
+	cin >> h >> r >> c;
+	h=pow(2,h);
+	dv(0, 0, h);
 }
