313ef0bebf76c1be0ef3f26a10a16f86a64d365b
sy46 <78065521+sy460129@users.noreply.github.com>
2023-03-28 08:24:00 +0900

    [Gold II] Title: 반도체 설계, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int>v;
+int main(){
+	int n, x;
+	cin >> n;
+	v.push_back(-1);
+	for(int i=0; i<n; i++){
+		cin >> x;
+		if(v.back()<x){
+			v.push_back(x);
+		}
+		else{
+			auto it=lower_bound(v.begin(), v.end(), x)-v.begin();
+			v[it]=x;
+		}
+	}
+	cout << v.size()-1;
+}
