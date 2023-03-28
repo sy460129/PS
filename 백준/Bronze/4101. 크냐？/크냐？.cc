357e222ed1b99c2e2aeb7dc77e8b89c36e09167a
sy46 <78065521+sy460129@users.noreply.github.com>
2023-03-28 11:20:01 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	while(1){
+		cin >> a >> b;
+		if(a==0 && b==0){
+			return 0;
+		}
+		if(a>b){
+			cout << "Yes\n";
+		}
+		else{
+			cout << "No\n";
+		}
+	}
+}
