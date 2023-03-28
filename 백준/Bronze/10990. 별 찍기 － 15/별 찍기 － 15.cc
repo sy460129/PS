971a87dabb9c6ec8e32383bade77c9f797c81436
sy46 <78065521+sy460129@users.noreply.github.com>
2023-03-28 22:12:01 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#define rep(i, a, b) for(int i=a; i<b; i++)
+using namespace std;
+int main() {
+	int n; cin >> n;
+	rep(i, 0, n){
+		rep(j, 0, n-i-1){
+			cout << " ";
+		}
+		cout << "*";
+		if(i==0) {
+			cout << "\n";
+			continue;
+		}
+		rep(j, 0, 2*i-1){
+			cout << " ";
+		}
+		cout << "*\n";
+	}
+}
