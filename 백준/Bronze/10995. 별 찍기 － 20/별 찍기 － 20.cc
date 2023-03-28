7a3ff1a25818e8458b48543e1704fe2515dce9dc
sy46 <78065521+sy460129@users.noreply.github.com>
2023-03-28 22:13:21 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<2*n; j++){
+			if(i%2!=0){
+				if(j%2!=0){
+					cout << "*";
+				}
+				else{
+					cout << " ";
+				}
+			}
+			else{
+				if(j%2==0){
+					cout << "*";
+				}
+				else{
+					cout << " ";
+				}
+			}
+		}
+		cout << "\n";
+	}
+}
