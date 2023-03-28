3609fd07e2c9916c44b6d9c9a0a366d225f20228
sy46 <78065521+sy460129@users.noreply.github.com>
2023-03-28 22:14:50 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x, a, b;
+	cin >> x;
+	for(int i=1; i<=x; i++){
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a+b << "\n";
+	}
+}
