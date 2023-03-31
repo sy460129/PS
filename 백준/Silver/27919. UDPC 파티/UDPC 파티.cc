9a12c517ca59c8895457251527158110c2bde87f
sy46 <78065521+sy460129@users.noreply.github.com>
2023-03-31 15:16:05 +0900

    [Silver V] Title: UDPC 파티, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Silver/27919. UDPC 파티/UDPC 파티.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string s;
+	int u=0, d=0;
+	cin >> s;
+	for(int i=0; i<s.length(); i++){
+		if(s[i]=='U' || s[i]=='C') u++;
+		else if(s[i]=='D' || s[i]=='P') d++;
+	}
+	if(u>(d+1)/2) cout << "U";
+	u=d=0;
+	for(int i=0; i<s.length(); i++){
+		if(s[i]=='D' || s[i]=='P') d++;
+	}
+	if(d>u) cout << "DP";
+}
