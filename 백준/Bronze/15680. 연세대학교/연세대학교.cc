51cf386c4fb3721897139899a5af48f8f03949f9
sy46 <78065521+sy460129@users.noreply.github.com>
2023-04-06 21:43:26 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	if(a==1){
+		cout<<"Leading the Way to the Future";
+	}
+	else{
+		cout<<"YONSEI";
+	}
+}
