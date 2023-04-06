9db5f868282029c251e18d4c8ec5dbe4576e2fcf
sy46 <78065521+sy460129@users.noreply.github.com>
2023-04-06 21:42:50 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m, x;
+bool check[9];
+vector<int>v, vec;
+void dfs(int t){
+	if(v.size()==m){
+		for(auto e : v){
+			cout << e << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	int p=-1;
+	for(int i=t; i<n; i++){
+		if(!check[i] && p!=vec[i]){
+			v.push_back(vec[i]);
+			check[i]=true;
+			p=vec[i];
+			dfs(i+1);
+			check[i]=false;
+			v.pop_back();
+		}
+	}
+}
+int main(){
+	cin >> n >> m;
+	for(int i=0; i<n; i++){
+		cin >> x;
+		vec.push_back(x);
+	}
+	sort(vec.begin(), vec.end());
+	dfs(0);
+}
