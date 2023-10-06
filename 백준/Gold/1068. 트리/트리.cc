f0ff120695da2d3d074ba05d06d70317516c87f8
sy46 <78065521+sy460129@users.noreply.github.com>
2023-10-06 16:58:32 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -2,31 +2,28 @@
 #include <vector>
 using namespace std;
 vector<int>v[51];
-int n, x, h, cnt;
+int n, t, x, cnt;
 void dfs(int cur){
-	if(v[cur].empty()){
-		cnt++;
-		return;
-	}
-	for(int i=0; i<v[cur].size(); i++){
-		if(v[cur][i]==x){
-			if(v[cur].size()==1) cnt++;
-		}
-		else dfs(v[cur][i]);
-	}
+    for(int i=0; i<v[cur].size(); i++){
+        if(v[cur][i]==x){
+            if(v[cur].size()==1) cnt++;
+        }
+        else dfs(v[cur][i]);
+    }
+    if(v[cur].size()==0) cnt++;
 }
 int main(){
-	cin >> n;
-	for(int i=0; i<n; i++){
-		cin >> x;
-		if(x==-1) h=i;
-		else v[x].push_back(i);
-	}
-	cin >> x;
-	if(x==h){
-		cout << 0;
-		return 0;
-	}
-	dfs(h);
-	cout << cnt;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> x;
+        if(x==-1) t=i;
+        else v[x].push_back(i);
+    }
+    cin >> x;
+    if(x==t){
+        cout << 0;
+        return 0;
+    }
+    dfs(t);
+    cout << cnt;
 }
