f75c8cb1f6c619ed945bd7a2568e49a00bff5bd8
sy46 <78065521+sy460129@users.noreply.github.com>
2023-12-15 16:17:06 +0900

    [Gold IV] Title: 알파벳, Time: 516 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,42 +1,31 @@
-#include<iostream>
-#include<queue>
-char arr[21][21];
-bool check[26];
-int dp[21][21];
-int dx[] = { 1,-1,0,0 };
-int dy[] = { 0,0,1,-1 };
-int n, m, t = 0;
+#include <iostream>
 using namespace std;
+int r, c, ans;
+char arr[21][21];
+bool visit[21][21], check[26];
+int dx[]={1,-1,0,0};
+int dy[]={0,0,1,-1};
 void dfs(int x, int y, int cnt) {
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (nx<0 || ny<0 || nx>n - 1 || ny>m - 1) {
-			continue;
-		}
-		if (check[arr[nx][ny] - 65] == true) {
-			if (cnt > t) {
-				t = cnt;
-			}
-			continue;
-		}
-		else {
-			check[arr[nx][ny] - 65] = true;
-			dfs(nx, ny, cnt + 1);
-			check[arr[nx][ny] - 65] = false;
-		}
+	ans=max(ans, cnt);
+	for(int i=0; i<4; i++){
+		int nx=x+dx[i], ny=y+dy[i];
+		if(nx<0 || ny<0 || nx>r-1 || ny>c-1 || visit[nx][ny] || check[arr[nx][ny]-'A']) continue;
+		check[arr[nx][ny]-'A']=true;
+		visit[nx][ny]=true;
+		dfs(nx,ny,cnt+1);
+		check[arr[nx][ny]-'A']=false;
+		visit[nx][ny]=false;
 	}
 }
-int main() {
-	string s;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> s;
-		for (int j = 0; j < m; j++) {
-			arr[i][j] = s[j];
+int main(){
+	cin >> r >> c;
+	for(int i=0; i<r; i++){
+		for(int j=0; j<c; j++){
+			cin >> arr[i][j];
 		}
 	}
-	check[arr[0][0] - 65] = true;
-	dfs(0, 0, 1);
-	cout << t << "\n";
+	check[arr[0][0]-'A']=true;
+	visit[0][0]=true;
+	dfs(0,0,1);
+	cout << ans;
 }
