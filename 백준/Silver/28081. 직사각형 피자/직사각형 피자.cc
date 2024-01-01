67cfc11e751749359a290e07725b091084b06284
sy46 <78065521+sy460129@users.noreply.github.com>
2024-01-01 13:42:59 +0900

    [Silver I] Title: 직사각형 피자, Time: 128 ms, Memory: 5888 KB -BaekjoonHub

--- 백준/Silver/28081. 직사각형 피자/직사각형 피자.cc
+++ 백준/Silver/28081. 직사각형 피자/직사각형 피자.cc
@@ -3,18 +3,17 @@
 #include <algorithm>
 using namespace std;
 using ll=long long;
-vector<ll>x, y, v, v2;
+int x[100001], y[100001];
+vector<ll>v, v2;
 int main(){
 	int w, h, n, m, cnt=1; 
 	ll k, ans=0; cin >> w >> h >> k;
 	cin >> n;
-	x.resize(n+1);
 	for(int i=1; i<=n; i++) cin >> x[i];
 	for(int i=0; i<n; i++) v.push_back(x[i+1]-x[i]);
 	v.push_back(h-x[n]);
 	sort(v.begin(), v.end());
 	cin >> m;
-	y.resize(m+1);
 	for(int i=1; i<=m; i++) cin >> y[i];
 	for(int i=0; i<m; i++) v2.push_back(y[i+1]-y[i]);
 	v2.push_back(w-y[m]);
