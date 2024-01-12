a538f7fd7ca7e7da8b0380bda1429392d84ee2ae
sy46 <78065521+sy460129@users.noreply.github.com>
2024-01-12 23:24:59 +0900

    [Silver II] Title: 개표, Time: 92 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24913. 개표/개표.cc
+++ 백준/Silver/24913. 개표/개표.cc
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
-typedef long long ll;
-ll arr[100001];
+using ll=long long;
+ll arr[100002];
+bool check;
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-	ll n, k, a, b, c;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	ll n, k, a, x, p, sum=0, c, mi=0;
 	cin >> n >> k;
 	while(k--){
-		bool check=false;
-		cin >> a >> b >> c;
-		if(a==1) arr[c]+=b;
+		cin >> a >> x >> p;
+		if(a==1){
+			arr[p]+=x;
+			if(p!=n+1) sum+=x, mi=max(mi,arr[p]);
+		}
 		else{
-			for(int i=1; i<=n; i++){
-				if(arr[i]+c<arr[n+1]+b){
-					check=true;
-					break;
-				}
-			}
-			!check ? cout << "NO\n" : cout << "YES\n";
+			c=(sum+p)/n;
+			if((sum+p)%n) c++;
+			c>=arr[n+1]+x || mi>=arr[n+1]+x ? cout << "NO\n" : cout << "YES\n";
 		}
 	}
 }
