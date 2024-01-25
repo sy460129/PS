46f18ae551ec8fd7f0b4648e1fa26885d2f9faf1
sy46 <78065521+sy460129@users.noreply.github.com>
2024-01-25 10:35:57 +0900

    [Gold III] Title: 프로그램, Time: 544 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/2900. 프로그램/프로그램.cc
+++ 백준/Gold/2900. 프로그램/프로그램.cc
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <set>
 #define MAX 1000001
 using namespace std;
 using ll=long long;
 ll dp[MAX];
 ll cnt[MAX];
-set<int>s;
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 	int n, k, x, l, r; cin >> n >> k;
 	while(k--){
 		cin >> x;
-		s.insert(x);
 		cnt[x]++;
 	}
-	for(auto e : s){
-		for(int i=1; i<=MAX; i+=e) dp[i-1]+=cnt[e];
+	for(int j=0; j<MAX; j++){
+		if(!cnt[j]) continue;
+		for(int i=0; i<MAX; i+=j) dp[i]+=cnt[j];
 	}
 	for(int i=1; i<MAX; i++) dp[i]+=dp[i-1];
 	cin >> x;
