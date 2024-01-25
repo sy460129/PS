4407749bb7a23c2d27d5592f34c085e8a277dac0
sy46 <78065521+sy460129@users.noreply.github.com>
2024-01-25 10:47:34 +0900

    [Platinum V] Title: KCM Travel, Time: 2896 ms, Memory: 23416 KB -BaekjoonHub

--- 백준/Platinum/10217. KCM Travel/KCM Travel.cc
+++ 백준/Platinum/10217. KCM Travel/KCM Travel.cc
@@ -5,32 +5,36 @@
 using namespace std;
 using ll=int;
 using p=pair<pair<ll,ll>,ll>;
-ll dist[102][10002];
+ll dist[102][10002]; // [공항][비용]
 ll t, n, m, k, u, v, c, d;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cin >> t;
     while(t--){
-        priority_queue<p, vector<p>, greater<p>>q;
+        queue<p>q;
         vector<p>vec[101];
         fill(dist[0], dist[102], INF);
         dist[1][0]=0;
         cin >> n >> m >> k;
         while(k--){
             cin >> u >> v >> c >> d;
-            vec[u].push_back({{v,d},c});
+            vec[u].push_back({{v,d},c}); // {{도착, 소요시간}, 비용} 
         }
-        q.push({{0,1},0});
+        q.push({{0,1},0}); // {{소요시간, 출발}, 비용}
         while(!q.empty()){
-            ll nd=q.top().first.first, nx=q.top().first.second, nc=q.top().second;
+            ll nd=q.front().first.first, nx=q.front().first.second, nc=q.front().second;
+            // nd = 소요시간, nx = 출발, nc = 비용
             q.pop();
-            for(int i=0; i<vec[nx].size(); i++){
-                ll next=vec[nx][i].first.first, dis=vec[nx][i].first.second, cost=vec[nx][i].second;
-                if(nc+cost>m) continue;
-                if(dist[next][nc+cost]>nd+dis){
-                    dist[next][nc+cost]=nd+dis;
-                    q.push({{dist[next][nc+cost],next},nc+cost});
+            if(dist[nx][nc]==nd){
+                for(int i=0; i<vec[nx].size(); i++){
+                    ll next=vec[nx][i].first.first, dis=vec[nx][i].first.second, cost=vec[nx][i].second;
+                    // next = 도착, dis = 소요시간, cost = 비용
+                    if(nc+cost>m) continue;
+                    if(dist[next][nc+cost]>nd+dis){
+                        dist[next][nc+cost]=nd+dis;
+                        if(next!=n) q.push({{dist[next][nc+cost],next},nc+cost});
+                    }
                 }
             }
         }
