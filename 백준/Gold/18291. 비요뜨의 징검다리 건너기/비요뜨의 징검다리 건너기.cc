6e67063063d2a0bcf6eef5070e76faf0be5f35c4
sy46 <78065521+sy460129@users.noreply.github.com>
2024-02-12 20:47:17 +0900

    [Gold V] Title: 비요뜨의 징검다리 건너기, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+typedef unsigned long long ull;
+ull MAX=1e9 + 7;
+ull multi(ull x, ull m){
+	if(m==0){
+		return 1;
+	}
+	else if(m==1){
+		return x;
+	}
+	else if(m%2!=0){
+		return multi(x, m-1)*x;
+	}
+	ull h=multi(x, m/2);
+	h%=MAX;
+	return (h*h)%MAX;
+}
+int main(){
+	ull t, n;
+	cin >> t;
+	while(t--){
+		cin >> n;
+		if(n==1){
+			cout << 1 << "\n";
+			continue;
+		}
+		cout << multi(2, n-2) % MAX << "\n";
+	}
+}
