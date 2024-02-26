66cba40f4541ab4504a3c3b471b5f74b759816bb
sy46 <78065521+sy460129@users.noreply.github.com>
2024-02-26 21:02:00 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	long long t=1, k, cnt=0; cin >> k;
+	while((t<<1)<=k) t<<=1, cnt++;
+	if(t==k){
+		cout << cnt%2;
+		return 0;
+	}
+	cnt=0;
+	while(k>2){
+		k-=t, cnt++;
+		while(k<=t) t>>=1;
+	}
+	k==1 ? cout << cnt%2 : cout << (cnt+1)%2;
+}
